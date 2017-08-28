e630a078c031b3878405154cf0163552a2729bb6
Douglas Bastos <douglashsb@gmail.com>
2017-08-28 01:22:02 -0300

    Implementando primeiros testes

--- news_app.py
+++ news_app.py
@@ -4,8 +4,10 @@ from blueprints.noticias import noticias_blueprint
 from db_config import db
 
 
-db.connect()
-app = Flask(__name__)
-app.config.from_object('settings')
+def create_app(config_filename=None):
+    db.connect()
+    app = Flask(__name__)
+    if config_filename:
+        app.config.from_pyfile(config_filename)
 
-app.register_blueprint(noticias_blueprint, url_prefix='/portal')
+    app.register_blueprint(noticias_blueprint, url_prefix='/portal')
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-from news_app import app
+from news_app import create_app
 
+app = create_app(config_filename='/home/douglas/workspace/acrux_flask/settings.py')
 app.run(debug=True, use_reloader=True)
